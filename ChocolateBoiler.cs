1e9e0468a39ebd21a28231b7cc85fb02b4c77ca8
MrPikokon-creator <63126813+MrPikokon-creator@users.noreply.github.com>
2021-11-14 00:20:40 +0200

    Add files via upload

---
+++ ChocolateBoiler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class ChocolateBoiler
+    {
+        private bool empty; // Заполнен ли нагреватель
+        private bool boiled; // Вскипячён ли шоколад
+        private static ChocolateBoiler uniqueInstance; // Хранение единственного экземпляра
+        private static object sync = new object(); // Объект для синхронизации потоков
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        // Создание и возвращение экземпляра
+        public static ChocolateBoiler getInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (sync)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        uniqueInstance = new ChocolateBoiler();
+                    }
+                }
+            }
+            
+            return uniqueInstance;
+        }
+        
+        // Заполение нагревателя шоколадной смесью
+        public void Fill()
+        {
+            lock (sync)
+            {
+                if (isEmpty()) // Нагреватель пуст
+                {
+                    empty = false;
+                    boiled = false;
+                    Console.WriteLine("Заполнение нагревателя шоколадной смесью");
+                }
+            }
+        }
+        
+        // Кипячение шоколадной смеси
+        public void Boil()
+        {
+            lock (sync)
+            {
+                if (!isEmpty() && !isBoiled()) // Нагреватель не пуст, шоколад не вскипячён
+                {
+                    boiled = true;
+                    Console.WriteLine("Кипячение шоколадной смеси");
+                }
+            }
+        }
+        
+        // Слив содержимого нагревателя
+        public void Drain()
+        {
+            lock (sync)
+            {
+                if (!isEmpty() && isBoiled()) // Нагреватель не пуст, шоколад вскипячён
+                {
+                    Console.WriteLine("Слив содержимого нагревателя\n");
+                    empty = true;
+                    boiled = false;
+                }
+            }
+        }
+        
+        // Заполнен ли нагреватель
+        private bool isEmpty()
+        {
+            return empty;
+        }
+        
+        // Вскипячён ли шоколад
+        private bool isBoiled()
+        {
+            return boiled;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    public class Program
+    {
+        private static void Call()
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+            
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+        }
+        
+        public static void Main()
+        {
+            Call();
+            
+            new Thread(new ThreadStart(Call)).Start();
+            new Thread(new ThreadStart(Call)).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
